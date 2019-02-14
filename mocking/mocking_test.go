6d8398edaeaff65d94561735ab0b81a389a500ba
davidrodriguez <david.rodriguez@williamhill.com>
2019-02-14 17:32:15 +0100

    checking the order of the calls

--- mocking/mocking_test.go
+++ mocking/mocking_test.go
@@ -1,35 +1,43 @@
 package mocking
 
 import (
-	"bytes"
+	"reflect"
 	"testing"
 )
 
-type SpySleeper struct {
-	Calls int
+type CountdownOperationsSpy struct {
+	Calls []string
 }
 
-func (s *SpySleeper) Sleep() {
-	s.Calls++
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
 }
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
+	countdownOperationsSpy := &CountdownOperationsSpy{}
 
-	Countdown(buffer, spySleeper)
+	Countdown(countdownOperationsSpy, countdownOperationsSpy)
 
-	got := buffer.String()
-	want := `3
-2
-1
-Go!`
-
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
+	want := []string{
+		sleep,
+		write,
+		sleep,
+		write,
+		sleep,
+		write,
+		sleep,
+		write,
 	}
 
-	if spySleeper.Calls != 4 {
-		t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
+	if !reflect.DeepEqual(want, countdownOperationsSpy.Calls) {
+		t.Errorf("wanted calls %v got %v", want, countdownOperationsSpy.Calls)
 	}
 }
