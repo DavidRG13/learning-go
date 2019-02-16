dc59c19564d9581338bcb8938ed4c88ad0d01632
davidrodriguez <david.rodriguez@williamhill.com>
2019-02-16 18:45:28 +0100

    Refactoring tests

--- sync/sync.go
+++ sync/sync.go
@@ -1,13 +1,13 @@
 package sync
 
 type Counter struct {
-	value int64
+	value int
 }
 
 func (c *Counter) Inc() {
 	c.value++
 }
 
-func (c *Counter) Value() int64 {
+func (c *Counter) Value() int {
 	return c.value
 }
